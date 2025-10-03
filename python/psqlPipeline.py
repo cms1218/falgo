566330e75a7566e0942c11bb9d7698b16964bc78
Caleb Singleton <cmsingle12@gmail.com>
2025-10-03 15:16:15 -0400

    began directly implementing psql interaction

--- python/psqlPipeline.py
+++ python/psqlPipeline.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import features
 import os
+import psycopg2
 
 def pipeline():
     # Pull in csv
@@ -24,13 +25,35 @@ def pipeline():
     df['Volume'] = pd.to_numeric(df['Volume'])
 
     # Add new columns to the dataframe of popular financial indicators
-    features.SMA(df, 10)
-    features.EMA(df, 10)
     features.pctChange(df)
     features.volatility(df, 10)
 
     print(df["Close"].dtype)
     print(df.tail())
+
+    #connect to psql database
+    conn = psycopg2.connect(
+        dbname="falgo",
+        user="postgres", 
+        password="121805++", 
+        host = 'localhost',
+        port=5432
+    )
+
+    cur = conn.cursor()
+
+    #create table for specific ticker in psql database
+    create = f"""
+    CREATE TABLE {filename} (
+        date DATE UNIQUE
+        open NUMERIC(10, 2)
+        high NUMERIC(10,2)
+        low NUMERIC(10, 2)
+        close NUMERIC(10, 2)
+        pctChange NUMERIC(10, 2)
+        volatility NUMERIC (10, 2)
+    )
+    """
 
     
 
